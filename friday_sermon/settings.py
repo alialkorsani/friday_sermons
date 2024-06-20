0485c8e5056d57636bef92a0eca5ec02da788e57
Ali Alkorsani <alialkorsani@gmail.com>
2024-06-20 23:52:15 +0300

    ff

--- friday_sermon/settings.py
+++ friday_sermon/settings.py
@@ -76,10 +76,10 @@ WSGI_APPLICATION = 'friday_sermon.wsgi.application'
 
 DATABASES = {
     'default': {
-        #  'ENGINE': 'django.db.backends.mysql',
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        #  'NAME': 'sermons', 'USER': 'root', 'PASSWORD': '', 'HOST': 'localhost', 'PORT': '3306'
+        'ENGINE': 'django.db.backends.mysql',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+         'NAME': 'sermons', 'USER': 'root', 'PASSWORD': '', 'HOST': 'localhost', 'PORT': '3306'
     }
 }
 
