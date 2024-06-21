07440936cd2611a66ee47ec9707c42d2de4a802a
Ali Alkorsani <alialkorsani@gmail.com>
2024-06-22 00:41:23 +0300

    dd

--- app/models.py
+++ app/models.py
@@ -1,16 +1,16 @@
-from django.contrib.auth.backends import UserModel
+from django.contrib.auth import get_user_model
 from django.core.validators import FileExtensionValidator
 from django.db import models
 
+UserModel = get_user_model()
 
 class Token(models.Model):
     token = models.CharField(max_length=100, primary_key=True)
-    user_id = models.OneToOneField(
+    user = models.OneToOneField(
         UserModel,
         on_delete=models.CASCADE,
         related_name="auth"
     )
-
 
 class New(models.Model):
     title = models.CharField(max_length=100)
@@ -18,50 +18,45 @@ class New(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-
 class NewImage(models.Model):
     item = models.ForeignKey(New, on_delete=models.CASCADE, related_name="files")
     image = models.ImageField(upload_to="images")
 
-
-HELPS_TYPES = {
-    'MG': ('MG', 'Marriage aid'),
-    'B': ('B', 'Building aid'),
-    'D': ('D', 'Pay off debts'),
-    'O': ('O', 'Other'),
-    'MD': ('MD', 'Medical aid'),
-    'R': ('R', 'Reduction of pays'),
-    'P': ('P', 'Paying the rent'),
-    'F': ('F', 'Feed and clothe')
-}
-HELPS_SOURCE = {
-    'I': ('I', 'Imam hussain holy shrain'),
-    'S': ('S', 'Sharia rights')
-}
-
+HELPS_TYPES = [
+    ('MG', 'Marriage aid'),
+    ('B', 'Building aid'),
+    ('D', 'Pay off debts'),
+    ('O', 'Other'),
+    ('MD', 'Medical aid'),
+    ('R', 'Reduction of pays'),
+    ('P', 'Paying the rent'),
+    ('F', 'Feed and clothe')
+]
+HELPS_SOURCE = [
+    ('I', 'Imam hussain holy shrain'),
+    ('S', 'Sharia rights')
+]
 
 class Help(models.Model):
     title = models.CharField(max_length=100)
     desc = models.CharField(max_length=1000)
     type = models.CharField(
         max_length=2,
-        choices=HELPS_TYPES.values(),
+        choices=HELPS_TYPES,
         default="O",
     )
     source = models.CharField(
         max_length=1,
-        choices=HELPS_SOURCE.values(),
+        choices=HELPS_SOURCE,
         default="I",
     )
     value = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-
 class HelpImage(models.Model):
     item = models.ForeignKey(Help, on_delete=models.CASCADE, related_name="files")
     image = models.ImageField(upload_to="images")
-
 
 class Lecture(models.Model):
     title = models.CharField(max_length=100)
@@ -70,20 +65,17 @@ class Lecture(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-
 class LectureVideo(models.Model):
-    item = models.ForeignKey(
+    item = models.OneToOneField(
         Lecture,
         on_delete=models.CASCADE,
-        primary_key=True,
         related_name="files"
     )
     image = models.ImageField(upload_to="videos")
 
-
 class FridaySermon(models.Model):
     title = models.CharField(max_length=100)
-    desc = models.CharField(max_length=100)
+    desc = models.CharField(max_length=100, default='default description')  # Add default value
     title_1 = models.CharField(max_length=100)
     title_2 = models.CharField(max_length=100)
     desc_1 = models.CharField(max_length=1000000)
@@ -93,18 +85,15 @@ class FridaySermon(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-
 class FridaySermonVideo(models.Model):
     item = models.ForeignKey(FridaySermon, on_delete=models.CASCADE, related_name="files")
     image = models.ImageField(upload_to="videos")
-
 
 class Image(models.Model):
     title = models.CharField(max_length=100)
     image = models.ImageField(upload_to="images")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
 
 class Status(models.Model):
     title = models.CharField(max_length=100)
@@ -113,11 +102,9 @@ class Status(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-
 class StatusImage(models.Model):
     item = models.ForeignKey(Status, on_delete=models.CASCADE, related_name="files")
     image = models.ImageField(upload_to="images")
-
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
@@ -126,25 +113,21 @@ class Book(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-
 class BookPDF(models.Model):
     item = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="files")
     image = models.ImageField(upload_to="images")
     pdf = models.FileField(upload_to="pdfs", validators=[
         FileExtensionValidator(allowed_extensions=['pdf'])])
 
-
 class Live(models.Model):
     url = models.URLField(max_length=200)
     title = models.CharField(max_length=200)
     desc = models.CharField(max_length=200)
 
-
 class Views(models.Model):
     type = models.CharField(max_length=200)
     model_id = models.IntegerField()
     date = models.DateTimeField(auto_now_add=True)
-
 
 class Stat(models.Model):
     label = models.CharField(max_length=255)
@@ -156,4 +139,4 @@ class Stat(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return str(self.year) + " - " + self.label + " - " + str(self.value) + " - " + str(self.count)
+        return f"{self.year} - {self.label} - {self.value} - {self.count}"
